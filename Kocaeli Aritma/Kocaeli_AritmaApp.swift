bb1f93f827668d8e668a47ba7d86f37cfaf47c69
Selchuk <selcuk_dag@hotmail.com>
2021-04-30 17:01:02 +0300

    Initial Commit

---
+++ Kocaeli Aritma/Kocaeli_AritmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kocaeli_AritmaApp.swift
+//  Kocaeli Aritma
+//
+//  Created by Furkan Selcuk Dag on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Kocaeli_AritmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
