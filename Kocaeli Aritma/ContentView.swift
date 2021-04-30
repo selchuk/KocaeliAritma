8e2c4869d7cbadd554d1c6e54ee3b2fdd3c3d15a
Selchuk <selcuk_dag@hotmail.com>
2021-04-30 22:06:18 +0300

    4- bugünlük bu kadar - arayüz ve alt sayfalar biraz daha ilerledi

--- Kocaeli Aritma/ContentView.swift
+++ Kocaeli Aritma/ContentView.swift
@@ -16,58 +16,74 @@ struct ContentView: View {
     
     var body: some View {
         
-        ScrollView{
-            LazyVStack{
-                ZStack {
-                    TabView(selection : self.$selectedTab){
-                        ZStack{
-                            Image("su").resizable()
+        GeometryReader{ g in
+            ScrollView{
+                LazyVStack{
+                    ZStack {
+                        TabView(selection : self.$selectedTab){
+                            ZStack{
+                                Image("su").resizable()
+                                
+                       
+                            }
+                            ZStack{
+                                Image("su4").resizable() //her Zstack'te resizable olursa kenara mıknatıs gibi yapışma olayı olmuyor
+                                VStack {
+                                    
+                                    Text("ATIKSU ARITMADA EKONOMİK ÇÖZÜMLER").font(.largeTitle).fontWeight(.regular).foregroundColor(.white).frame(height: g.size.height/3, alignment: .bottom).shadow(radius: 10).multilineTextAlignment(.center)
+                                        Spacer()
+                                }
+                            }
+                            
+                        }.tabViewStyle(PageTabViewStyle()).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                        
+                        ZStack { //düğmeler ve ortadaki logo için
                             
                             VStack {
-                                Spacer()
-                                Text("SU KULLANARAK DOĞAYI KİRLETMEYİN, ARITMA TESİSİNİZİ KURUN!").font(.largeTitle).fontWeight(.regular).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/4.5, alignment: .top).shadow(radius: 10).multilineTextAlignment(.center)
-                            }
-                        }
-                        ZStack{
-                            Image("tesis").resizable() //her Zstack'te resizable olursa kenara mıknatıs gibi yapışma olayı olmuyor
-                            VStack {
-                                
-                                Text("ATIKSU ARITMADA EKONOMİK ÇÖZÜMLER").font(.largeTitle).fontWeight(.regular).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/3, alignment: .bottom).shadow(radius: 10).multilineTextAlignment(.center)
+                                HStack {
+                                    Image(systemName: "line.horizontal.3").padding(.leading, g.size.width/20).padding(.top, g.size.height/20)
                                     Spacer()
-                            }
+                                }
+                                
+                                Spacer()
+                                
+                                HStack { //en alt satir
+                                    Image(systemName: "questionmark.circle.fill").padding(g.size.width/20)
+                                    Spacer()
+                                    Image(systemName: "magnifyingglass.circle.fill").padding(g.size.width/20)
+                                    Spacer()
+                                    Image(systemName: "location.circle.fill").padding(g.size.width/20)
+                                    Spacer()
+                                    Image(systemName: "phone.circle.fill").padding(g.size.width/20)
+                                }
+                                
+                            }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .topLeading).foregroundColor(.white).font(.largeTitle).shadow(radius: 10)
+                            
+                            Image("sitelogo").resizable().scaledToFit().padding(g.size.height/8)
+                            
                         }
                         
-                    }.tabViewStyle(PageTabViewStyle()).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                    }//Zstack çıkış
                     
-                    ZStack { //düğmeler ve ortadaki logo için
+                    ZStack {
+                        Image("su2").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
                         
                         VStack {
-                            HStack {
-                                Image(systemName: "line.horizontal.3").padding(30)
-                                Spacer()
-                            }
                             
+                            Text("SU KULLANARAK DOĞAYI KİRLETMEYİN, ARITMA TESİSİNİZİ KURUN!").font(.largeTitle).fontWeight(.light).foregroundColor(.gray).frame(height: UIScreen.main.bounds.height/3, alignment: .center).shadow(radius: 10).multilineTextAlignment(.center)
                             Spacer()
-                            
-                            HStack { //en alt satir
-                                Image(systemName: "questionmark.circle.fill").padding(30)
-                                Spacer()
-                                Image(systemName: "magnifyingglass.circle.fill").padding(30)
-                                Spacer()
-                                Image(systemName: "location.circle.fill").padding(30)
-                                Spacer()
-                                Image(systemName: "phone.circle.fill").padding(30)
-                            }
-                            
-                        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .topLeading).foregroundColor(.white).font(.largeTitle).shadow(radius: 10)
-                        
-                        Image("sitelogo").resizable().scaledToFit().padding(70)
-                        
+                        }
                     }
                     
-                }//Zstack çıkış
-            }
-        }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Image("su3").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                    
+                    
+                    
+                    
+                    
+                }
+            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
