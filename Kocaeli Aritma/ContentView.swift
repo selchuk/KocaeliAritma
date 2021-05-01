d9acda4ae3eb04e838e33f3a770f71e411124d14
Selchuk <selcuk_dag@hotmail.com>
2021-05-01 17:51:53 +0300

    yeni sayfalar eklendi

--- Kocaeli Aritma/ContentView.swift
+++ Kocaeli Aritma/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     
     @State var selectedTab = 0
-    @State var seciliTabZorla = "0"
+    @State var seciliTabZorla = 0
     
     
     var body: some View {
@@ -19,67 +19,68 @@ struct ContentView: View {
         GeometryReader{ g in
             ScrollView{
                 LazyVStack{
-                    ZStack {
-                        TabView(selection : self.$selectedTab){
-                            ZStack{
-                                Image("su").resizable()
-                                
-                       
-                            }
-                            ZStack{
-                                Image("su4").resizable() //her Zstack'te resizable olursa kenara mıknatıs gibi yapışma olayı olmuyor
-                                VStack {
+                        
+                        ZStack {
+                            TabView(selection : self.$selectedTab){
+                                ZStack{
+                                    Image("anasayfa").resizable()
                                     
-                                    Text("ATIKSU ARITMADA EKONOMİK ÇÖZÜMLER").font(.largeTitle).fontWeight(.regular).foregroundColor(.white).frame(height: g.size.height/3, alignment: .bottom).shadow(radius: 10).multilineTextAlignment(.center)
-                                        Spacer()
+                                    
                                 }
+                                ZStack{
+                                    Image("su").resizable() //her Zstack'te resizable olursa kenara mıknatıs gibi yapışma olayı olmuyor
+                                    VStack {
+                                        
+                                        Text("ATIKSU ARITMADA EKONOMİK ÇÖZÜMLER").font(.largeTitle).fontWeight(.regular).foregroundColor(.white).frame(height: g.size.height/3, alignment: .bottom).shadow(radius: 10).multilineTextAlignment(.center)
+                                        Spacer()
+                                    }
+                                }
+                                
+                            }.tabViewStyle(PageTabViewStyle()).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                            
+                            
+                            ZStack { //düğmeler ve ortadaki logo için
+                                
+                                VStack {
+                                    HStack {
+                                        Image(systemName: "line.horizontal.3").padding(.leading, g.size.width/20).padding(.top, g.size.height/20)
+                                        Spacer()
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    HStack { //en alt satir
+                                        Image(systemName: "questionmark.circle").padding(g.size.width/20)
+                                        Spacer()
+                                        Image(systemName: "magnifyingglass.circle").padding(g.size.width/20)
+                                        Spacer()
+                                        Image(systemName: "location.circle").padding(g.size.width/20)
+                                        Spacer()
+                                        Image(systemName: "phone.circle").padding(g.size.width/20)
+                                    }
+                                    
+                                }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .topLeading).foregroundColor(.white).font(.largeTitle).shadow(radius: 10)
+                                
+                                Image("sitelogo").resizable().scaledToFit().padding(g.size.height/12)
+                                
                             }
                             
-                        }.tabViewStyle(PageTabViewStyle()).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                        }//Zstack çıkış
                         
-                        ZStack { //düğmeler ve ortadaki logo için
+                        ZStack {
+                            Image("su").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
                             
                             VStack {
-                                HStack {
-                                    Image(systemName: "line.horizontal.3").padding(.leading, g.size.width/20).padding(.top, g.size.height/20)
-                                    Spacer()
-                                }
                                 
+                                Text("SU KULLANARAK DOĞAYI KİRLETMEYİN, ARITMA TESİSİNİZİ KURUN!").font(.largeTitle).fontWeight(.light).foregroundColor(.gray).frame(height: UIScreen.main.bounds.height/3, alignment: .center).shadow(radius: 10).multilineTextAlignment(.center)
                                 Spacer()
-                                
-                                HStack { //en alt satir
-                                    Image(systemName: "questionmark.circle.fill").padding(g.size.width/20)
-                                    Spacer()
-                                    Image(systemName: "magnifyingglass.circle.fill").padding(g.size.width/20)
-                                    Spacer()
-                                    Image(systemName: "location.circle.fill").padding(g.size.width/20)
-                                    Spacer()
-                                    Image(systemName: "phone.circle.fill").padding(g.size.width/20)
-                                }
-                                
-                            }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .topLeading).foregroundColor(.white).font(.largeTitle).shadow(radius: 10)
-                            
-                            Image("sitelogo").resizable().scaledToFit().padding(g.size.height/8)
-                            
+                            }
                         }
                         
-                    }//Zstack çıkış
-                    
-                    ZStack {
-                        Image("su2").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
-                        
-                        VStack {
-                            
-                            Text("SU KULLANARAK DOĞAYI KİRLETMEYİN, ARITMA TESİSİNİZİ KURUN!").font(.largeTitle).fontWeight(.light).foregroundColor(.gray).frame(height: UIScreen.main.bounds.height/3, alignment: .center).shadow(radius: 10).multilineTextAlignment(.center)
-                            Spacer()
-                        }
-                    }
-                    
-                    Image("su3").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                        Image("tesis").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
                     
                     
-                    
-                    
+                    //dikey tabview cikisi
                     
                 }
             }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -87,8 +88,16 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews_Default: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        Group {
+            ContentView().previewDevice("iPhone SE")
+            ContentView().previewDevice("iPhone 12 Pro Max")
+                .preferredColorScheme(.dark)
+            
+        }
+        
     }
 }
+
