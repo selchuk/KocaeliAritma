ae5b1ce8dfd6c703a79f688f4ec3ac1b288c4b73
Selchuk <selcuk_dag@hotmail.com>
2021-05-02 16:41:41 +0300

    süper 01

--- Kocaeli Aritma/ContentView.swift
+++ Kocaeli Aritma/ContentView.swift
@@ -96,11 +96,12 @@ struct ContentView: View {
                         }//Zstack çıkış
                         
                         ZStack {
-                            Image("su").resizable().frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                            Image("su").resizable().aspectRatio(contentMode: .fill).frame(width: .infinity, height: UIScreen.main.bounds.height, alignment: .center)
+                                .clipped()
                             
                             VStack {
                                  Spacer()
-                                Text("Arıtma").font(.custom("Foral Pro", size: g.size.width/7, relativeTo: .headline)).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/2, alignment: .bottom).multilineTextAlignment(.center)
+                                Text("Arıtma").font(.custom("Foral Pro", size: g.size.width/8, relativeTo: .headline)).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/2, alignment: .bottom).multilineTextAlignment(.center)
                                     .shadow(color: .black, radius: 1, x: 1, y: 1)
                                     .padding(10)
                                 Text("DAHA FAZLA BİLGİ ").font(.custom("Avenir Next", size: g.size.width/20, relativeTo: .headline)).fontWeight(.light).foregroundColor(.white).multilineTextAlignment(.center)
@@ -111,7 +112,39 @@ struct ContentView: View {
                             }
                         }
                         
-//charter    futura
+                    ZStack {
+                        Image("kimyasallar2").resizable().aspectRatio(contentMode: .fill).frame(width: .infinity, height: UIScreen.main.bounds.height, alignment: .center)
+                            .clipped()
+                        
+                        VStack {
+                            Text("Kimyasallar").font(.custom("Foral Pro", size: g.size.width/8, relativeTo: .headline)).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/5, alignment: .bottom).multilineTextAlignment(.center)
+                                .shadow(color: .black, radius: 1, x: 1, y: 1)
+                                .padding(3)
+                            Text("DAHA FAZLA BİLGİ ").font(.custom("Avenir Next", size: g.size.width/20, relativeTo: .headline)).fontWeight(.light).foregroundColor(.white).multilineTextAlignment(.center)
+                           //     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 1, x: 1, y: 1)
+                                .frame(width: g.size.width*0.6, height: g.size.width/10, alignment: .center)
+                                .border(Color.white, width: 0.3)
+                            Spacer()
+                        }
+                    }
+                    
+                    
+                    ZStack {
+                        Image("hizmetlerimiz").resizable().aspectRatio(contentMode: .fill).frame(width: .infinity, height: UIScreen.main.bounds.height, alignment: .center)
+                            .clipped()
+                        
+                        VStack {
+                            Spacer()
+                            Text("Hizmetlerimiz").font(.custom("Foral Pro", size: g.size.width/8, relativeTo: .headline)).foregroundColor(.white).frame(height: UIScreen.main.bounds.height/5, alignment: .bottom).multilineTextAlignment(.center)
+                                .shadow(color: .black, radius: 1, x: 1, y: 1)
+                                .padding(3)
+                            Text("DAHA FAZLA BİLGİ ").font(.custom("Avenir Next", size: g.size.width/20, relativeTo: .headline)).fontWeight(.light).foregroundColor(.white).multilineTextAlignment(.center)
+                           //     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 1, x: 1, y: 1)
+                                .frame(width: g.size.width*0.6, height: g.size.width/10, alignment: .center)
+                                .border(Color.white, width: 0.3)
+                            Spacer()
+                        }
+                    }
                     
                     
                     //dikey tabview cikisi
